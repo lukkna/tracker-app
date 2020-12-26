119ce758fa207428e2985a2b55a32cc76c808fb0
Lukas <knasaslukas@gmail.com>
2020-12-26 16:12:52 +0200

    help text

--- app/src/main/java/eu/vk/trackerapp/ui/help/HelpViewModel.java
+++ app/src/main/java/eu/vk/trackerapp/ui/help/HelpViewModel.java
@@ -5,12 +5,15 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class HelpViewModel extends ViewModel {
-
     private MutableLiveData<String> mText;
 
     public HelpViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Čia bus help");
+        mText.setValue("Progresas\n\n" +
+                "Suvesk dabartines savo apimtis, kad po to galėtum stebėti kaip jos keičiasi. \n\n" +
+                "  • Apimtis matuok ryte nieko nevalgius.\n" +
+                "  • Raumenys turi būti įtempti.\n" +
+                "  • Apimtys matuojamos kas savaitę.\n");
     }
 
     public LiveData<String> getText() {
